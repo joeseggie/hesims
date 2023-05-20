f6336a087e5166999e82421a34c82b58bdd96856
Joseph Serunjogi <joeseggie@gmail.com>
2023-05-20 13:01:09 +0300

    Add application cycle service

---
+++ src/dev/HESIMS/HESIMS.Web/Data/Services/ApplicationCycleService.cs
@@ -0,0 +1,72 @@
+namespace HESIMS.Web.Data.Services;
+
+/// <summary>
+/// Application cycle service interface.
+/// </summary>
+public interface IApplicationCycleService
+{
+    /// <summary>
+    /// Get application cycles.
+    /// </summary>
+    Task<IEnumerable<ApplicationCycle>> GetApplicationCyclesAsync();
+
+    /// <summary>
+    /// Add new application cycle.
+    /// </summary>
+    /// <param name="applicationCycle">New application cycle details</param>
+    Task AddApplicationCycleAsync(ApplicationCycle applicationCycle);
+
+    /// <summary>
+    /// Update application cycle details.
+    /// </summary>
+    /// <param name="applicationCycle">Application cycle update details.</param>
+    /// <returns>updated application cycle details.</returns>
+    Task<ApplicationCycle> UpdateApplicationCycleAsync(ApplicationCycle applicationCycle);
+
+    /// <summary>
+    /// Get application cycle by Id.
+    /// </summary>
+    /// <param name="id">Application cycle ID.</param>
+    /// <returns>Application cycle if ID exist, otherwise null</returns>
+    Task<ApplicationCycle?> GetApplicationCycleByIdAsync(Guid id);
+}
+
+public class ApplicationCycleService : IApplicationCycleService
+{
+    private readonly ApplicationDbContext db;
+
+    ///<summary>
+    /// Initializes a new instance of the <see cref="ApplicationCycleService"/> class.
+    /// </summary>
+    /// <param name="dbContext">Application DB context.</param>
+    public ApplicationCycleService(ApplicationDbContext dbContext)
+    {
+        db = dbContext;
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<ApplicationCycle>> GetApplicationCyclesAsync()
+    {
+        return await db.ApplicationCycles.ToListAsync();
+    }
+    
+    /// <inheritdoc/>
+    public async Task AddApplicationCycleAsync(ApplicationCycle applicationCycle)
+    {
+        await db.ApplicationCycles.AddAsync(applicationCycle);
+    }
+
+    /// <inheritdoc/>
+    public async Task<ApplicationCycle> UpdateApplicationCycleAsync(ApplicationCycle applicationCycle)
+    {
+        db.ApplicationCycles.Update(applicationCycle);
+        await db.SaveChangesAsync();
+        return applicationCycle;
+    }
+
+    /// <inheritdoc/>
+    public async Task<ApplicationCycle?> GetApplicationCycleByIdAsync(Guid id)
+    {
+        return await db.ApplicationCycles.FindAsync(id);
+    }
+}
